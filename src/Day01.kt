bfa73e76f62f005d3f0697edb1a32ed44cc3aace
thpz2210 <thomas.paetz@gmx.net>
2023-12-03 00:29:16 +0100

    solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        fun String.firstDigit() = this.find { it.isDigit() }
+        fun String.lastDigit() = this.findLast { it.isDigit() }
+        return input.sumOf { "${it.firstDigit()}${it.lastDigit()}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        fun Pair<Int, String>?.toInt() = digits.indexOf(this!!.second) % 9 + 1
+        fun String.firstDigit() = this.findAnyOf(digits).toInt()
+        fun String.lastDigit() = this.findLastAnyOf(digits).toInt()
+        return input.sumOf { 10 * it.firstDigit() + it.lastDigit() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("day01_test_1")
+    part1(testInput1).check(142)
 
-    val input = readInput("Day01")
+    val testInput2 = readInput("day01_test_2")
+    part2(testInput2).check(281)
+
+    val input = readInput("day01")
     part1(input).println()
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,16 +6,23 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun readInput(name: String) = Path("data/$name.txt").readLines()
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * The cleaner shorthand for checking a solution.
+ */
+fun Any?.check(expected: Any?) = check(this == expected) { "$this does not match expected $expected" }
+
+/**
+ * Extension properties for head and tail
+ */
+val <T> List<T>.tail: List<T>
+    get() = drop(1)
+
+val <T> List<T>.head: T
+    get() = first()
