cd8684d0e0c3ddf3c69f7b911263dcaedc54a15f
thpz2210 <thomas.paetz@gmx.net>
2024-01-02 22:57:09 +0100

    solved day 12
    part 1: ok
    part 2: too slow

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,66 +1,57 @@
 fun main() {
 
-    class Row(val row: MutableList<Char>, val expectedGroups: List<Int>) {
+    class Row(val condition: String, val expected: List<Int>) {
 
-        var possibleArrangements = 0
+        val count: Int
+            get() = count(0, listOf(condition))
 
-        fun actualGroups() = row.takeWhile { it != '?' }.fold(mutableListOf(0)) { acc, c ->
-            if (c == '#')
-                acc[acc.lastIndex] += 1
-            else
-                acc.add(0)
-            acc
-        }.filter { it > 0 }
-
-        fun fillArrangements() {
-            val pivot = row.indexOfFirst { it == '?' }
-
-            if (pivot == -1) {
-                if (actualGroups() == expectedGroups)
-                    possibleArrangements += 1
-                return
-            }
-
-            val actualGroups = actualGroups()
-
-            if (actualGroups.size > expectedGroups.size)
-                return
-
-            if (actualGroups.size > 1 && actualGroups.subList(0, actualGroups.size - 1) != expectedGroups.subList(0, actualGroups.size - 1))
-                return
-
-            row[pivot] = '#'
-            fillArrangements()
-            row[pivot] = '.'
-            fillArrangements()
-            row[pivot] = '?'
+        private fun String.matches(): Boolean {
+            val actual = substringBefore('?').fold(mutableListOf(0)) { acc, c ->
+                if (c == '#')
+                    acc[acc.lastIndex] += 1
+                else if (acc.last() > 0)
+                    acc.add(0)
+                acc
+            }.filter { it > 0 }
+            if ('?' !in this)
+                return actual == expected
+            if (actual.size > expected.size)
+                return false
+            if (actual.size > 1 && (0..<actual.lastIndex).any { actual[it] != expected[it] })
+                return false
+            return true
         }
 
-        fun possibleArrangements(): Int {
-            fillArrangements()
-            return possibleArrangements
+        private tailrec fun count(completeCount: Int, conditions: List<String>): Int {
+            val (incomplete, complete) = conditions.partition { '?' in it }
+            if (incomplete.isEmpty())
+                return completeCount + complete.count { it.matches() }
+            return count(
+                completeCount + complete.count { it.matches() },
+                incomplete.flatMap {
+                    listOf(
+                        it.substringBefore('?') + '#' + it.substringAfter('?'),
+                        it.substringBefore('?') + '.' + it.substringAfter('?')
+                    )
+                }.filter { it.matches() })
         }
     }
 
-    fun List<String>.toRows() =
-        map { Row(it.split(" ")[0].toMutableList(), it.split(" ")[1].split(",").map { c -> c.toInt() }) }
-
-    fun List<String>.unfold() = map {
-        val conditions = it.split(" ")[0]
-        val groups = it.split(" ")[1]
-        ("$conditions?") + ("$conditions?") + ("$conditions?") + ("$conditions?") + ("$conditions ") +
-                ("$groups,") + ("$groups,") + ("$groups,") + ("$groups,") + groups
+    fun List<String>.toRows() = map { line ->
+        val split = line.split(" ", ",")
+        Row(split.head, split.tail.map { it.toInt() })
     }
 
-    fun part1(input: List<String>) = input.toRows().mapIndexed { index, row ->
-        println(index)
-        row.possibleArrangements()
-    }.sum()
+    fun List<Row>.unfold() = map { row ->
+        Row(
+            (1..5).joinToString("?") { row.condition },
+            (1..5).flatMap { row.expected }
+        )
+    }
 
-    fun part2(input: List<String>) = input.unfold().toRows().mapIndexed { index, row ->
-        println(index)
-        row.possibleArrangements()
-    }.sum()
+    fun part1(input: List<String>) = input.toRows().sumOf { it.count }
+
+    fun part2(input: List<String>) = input.toRows().unfold().sumOf { it.count }
 
     val testInput = readInput("day12_test")
     part1(testInput).check(21)
@@ -68,5 +59,5 @@ fun main() {
 
     val input = readInput("day12")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println()
 }
